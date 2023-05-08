68a5a3d2cf6f23c6076b1a3397c63d329c1586da
Hossain Razavi <hossain.razavi@gmail.com>
2023-05-08 15:06:02 +0330

    Resonponsive fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
 
   return (
     <>
-      <div className="flex gap-8 items-center mt-16 w-[80vw] lg:w-[40vw] mx-auto">
-        <h1 className="text-6xl font-extrabold">ماشین حساب ابجد</h1>
+      <div className="flex flex-col-reverse md:flex-row gap-8 items-center mt-16 w-[80vw] lg:w-[40vw] mx-auto">
+        <h1 className="text-4xl md:ext-6xl font-extrabold">ماشین حساب ابجد</h1>
         <img src={logo} alt="Abjad Calculator" className="h-20" />
       </div>
       <div className="mt-12" dir="rtl">
-        <h2 className="text-center my-4 text-md lg:text-xl">جهت محاسبه جمع مقدار عددی حروف بر اساس سیستم ابجد، کلمه مورد نظر خود را وارد کنید:</h2>  
+        <h2 className="text-center my-4 px-6 text-md lg:text-xl">جهت محاسبه جمع مقدار عددی حروف بر اساس سیستم ابجد، کلمه مورد نظر خود را وارد کنید:</h2>  
         <Calculator />
       </div>
 
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -7,7 +7,7 @@ function Calculator({}: Props) {
   const [value, setValue] = useState("");
   const [sum, setSum] = useState(0);
 
-  //Get abjad value of a character based on values defined in AbjadValues
+  //Get abjad value of a character based on values defined in AbjadValues file
   const getLetterValue = (letter: string): number => {
     return letterValues[letter] || 0;
   };
@@ -18,7 +18,7 @@ function Calculator({}: Props) {
     return (
       (charCode >= 0x0600 && charCode <= 0x06ff) ||
       (charCode >= 0x0750 && charCode <= 0x077f) ||
-      (charCode === 0x0020)
+      charCode === 0x0020
     );
   }
 
@@ -46,8 +46,6 @@ function Calculator({}: Props) {
     setSum(calculateSum(inputValue));
   };
 
-
-
   return (
     <div className="flex flex-col items-center" dir="rtl">
       <input
@@ -55,12 +53,29 @@ function Calculator({}: Props) {
         value={value}
         onKeyDown={handleKeyDown}
         onChange={handleChange}
-        className="w-[80vw] md:w-[60vw] h-16 rounded-2xl px-6 py-4 text-black text-xl focus:outline outline-cyan-500 outline-8"
+        className="w-[80vw] md:w-[60vw] h-16 rounded-2xl px-6 py-4 text-black text-xl focus:outline outline-cyan-500 outline-8 placeholder:text-xs md:placeholder:text-xl"
         placeholder="لطفاً **فقط** از حروف فارسی یا عربی استفاده کنید"
       />
-      <p className="outline outline-green-300 text-green-300 rounded-full w-36 h-36 text-center m-8 text-8xl flex items-center ">
-        {sum}
-      </p>
+      <div className="flex items-center">
+        <svg
+          aria-hidden="true"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={1.5}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-16 animate-pulse"
+        >
+          <path
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <p className="mt-8 text-8xl text-emerald-600 p-4">
+          {sum}
+        </p>
+      </div>
     </div>
   );
 }
